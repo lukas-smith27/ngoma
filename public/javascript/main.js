491bc32de5bd5387d2c4fd777ae7f21043ad7651
Lukas Smith <lmssmith2000@gmail.com>
2023-02-22 12:18:31 -0500

    Page Redirect Change

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,107 +1,5 @@
 $(document).ready(function(){
-
-    history.pushState({page: 1}, "../public/home.html")
-    history.pushState({page: 2}, "../public/about.html")
-    history.pushState({page: 3}, "../public/wickerFamily.html")
-    history.pushState({page: 4}, "../public/projects.html")
-    history.pushState({page: 5}, "../public/womenChildren.html")
-    history.pushState({page: 6}, "../public/youthProjects.html")
-    history.pushState({page: 7}, "../public/enSchools.html")
-    history.pushState({page: 8}, "../public/involvement.html")
-    history.pushState({page: 9}, "../public/contact.html")
-    history.pushState({page: 9}, "../public/join.html")
-
     $(window).load("../public/home.html")
-
-    $(".logo-home").click(function(){
-        url = "../public/home.html"
-        window.location.replace(url)
-    })
-
-    $(".homeTop").click(function(){
-        url = "../public/home.html"
-        window.location.replace(url)
-    })
-
-    $(".aboutTop").click(function(){
-        url = "../public/about.html"
-        window.location.replace(url)
-    })
-
-    $("#aboutHome").click(function(){
-        url = "../public/about.html"
-        window.location.replace(url)
-    })
-
-    $(".wickerFamilyTop").click(function(){
-        url = "../public/wickerFamily.html"
-        window.location.replace(url)
-    })
-
-    $(".projectsTop").click(function(){
-        url = "../public/projects.html"
-        window.location.replace(url)
-    })
-
-    $("#projectsHome").click(function(){
-        url = "../public/projects.html"
-        window.location.replace(url)
-    })
-
-    $(".womenChildrenTop").click(function(){
-        url = "../public/womenChildren.html"
-        window.location.replace(url)
-    })
-
-    $(".womenChildren").click(function(){
-        url = "../public/womenChildren.html"
-        window.location.replace(url)
-    })
-
-    $(".youthTop").click(function(){
-        url = "../public/youthProjects.html"
-        window.location.replace(url)
-    })
-
-    $(".youth").click(function(){
-        url = "../public/youthProjects.html"
-        window.location.replace(url)
-    })
-
-    $(".enSchoolsTop").click(function(){
-        url = "../public/enSchools.html"
-        window.location.replace(url)
-    })
-
-    $(".eastNobleSchools").click(function(){
-        url = "../public/enSchools.html"
-        window.location.replace(url)
-    })
-
-    $(".involvementTop").click(function(){
-        url = "../public/involvement.html"
-        window.location.replace(url)
-    })
-
-    $("#involvementHome").click(function(){
-        url = "../public/involvement.html"
-        window.location.replace(url)
-    })
-
-    $(".contactTop").click(function(){
-        url = "../public/contact.html"
-        window.location.replace(url)
-    })
-
-    $(".joinTop").click(function(){
-        url = "../public/join.html"
-        window.location.replace(url)
-    })
-
-    $("#joinButton").click(function(){
-        url = "../public/join.html"
-        window.location.replace(url)
-    })
 
     // Menu Bars when screen is under X amount of pixels
     $(".small-menu").click(function(){
@@ -112,11 +10,6 @@ $(document).ready(function(){
             window.location.scrollTop()
         }
     })
-
-    let $anchors = $(".links li")
-    let $anchors2 = $(".links li ul li a")
-    $(".aboutTop").click(function(){ return false })
-    $(".servicesTop").click(function(){ return false })
 
     $($anchors).click(function(){
         $(".links").removeClass("active")
