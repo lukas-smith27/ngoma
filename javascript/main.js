e5708d9b1a5ba7fc5dd5b76129019e045bff6e49
Lukas Smith <lmssmith2000@gmail.com>
2023-03-20 14:25:30 -0400

    JS Edit

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,10 @@
-$(document).ready(function(){
-    window.location = "https://ngomacenterinc.com"
-    // Menu Bars when screen is under X amount of pixels
-    $(".small-menu").click(function(){
-        if (!$(".links").hasClass("active")) {
-            $(".links").addClass("active")
-        } else {
-            $(".links").removeClass("active")
-            window.location.scrollTop()
-        }
-    })
+// Menu Bars when screen is under X amount of pixels
+$(".small-menu").click(function(){
+    if (!$(".links").hasClass("active")) {
+        $(".links").addClass("active")
+    } else {
+        $(".links").removeClass("active")
+        window.location.scrollTop()
+    }
 })
 
